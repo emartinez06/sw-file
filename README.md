abfcf94b99352425dd939424fefb0bc53a81c87b
emartinez06 <edwin@devmartinez.net>
2018-12-07 14:40:57 -0400

    -sw-wrapper: Update to Readme file with how to use instructions. Update on License file to have full name on copyright notice. Addition of gitignore file. Addition of index.html file to test the service worker with example.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# sw-wrapper
+# Service Worker
 A service worker file to use on your projects. 
+
+### How to use
+1. Add this code snippet to your HTML files: ```<script>
+                                                         if ('serviceWorker' in navigator) {
+                                                             navigator.serviceWorker.register('sw.js', {scope: '/'})
+                                                                 .then(function(regSW) {
+                                                                     // registration worked
+                                                                     console.log('Registration succeeded. Scope is ' + regSW.scope);
+                                                                 }).catch(function(error) {
+                                                                 // registration failed
+                                                                 console.log('Registration failed with ' + error);
+                                                             });
+                                                         }else{
+                                                             alert('Cache not supported on your browser, please upgrade to a current one');
+                                                             location.replace("https://browsehappy.com/");
+                                                         }
+                                                     </script>```.
+2. Open the sw.js file and add all the files that you want to be cached inside the files array variable. Example: ```let files = ['/','.index.html'];```. 
+The *cacheName* variable is the name that your cache storage will have.
+Example: ```let cacheName = 'my-cache-name'```.
+3. Done. Now you have a service worker ready to use.
---
+++ sw.js
@@ -0,0 +1,62 @@
+//Some parts of this code are from Google documentation
+//URL: https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
+
+//Let's cache all necessary files (JS, HTML, CSS, Images, Maps, Data)
+let files = [
+    // Files and root
+    '/',
+    'index.html',
+    'sw.js'
+];
+let cacheName = 'my-app';
+
+//Install the service worker and open the cache
+self.addEventListener('install', function(event) {
+    console.log('[ServiceWorker] Installing');
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(files);
+        }).catch(function(err) {
+            console.log('Error on installing: ' + err);
+        })
+    )
+});
+
+//Activate and refresh service worker
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activated');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+//Fetch files and save them on storage
+self.addEventListener('fetch', function(event) {
+    console.log('[ServiceWorker] Fetching files');
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request).then(function(res) {
+                    return caches.open(cacheName)
+                        .then(function(cache) {
+
+                            //save the response for future
+                            cache.put(event.request.url, res.clone());
+                            //return the cached data
+                            return res;
+                        }).catch(function(err){
+                            console.log('Error on fetch: ' + err);
+                        });
+                })
+            })
+    );
+});
